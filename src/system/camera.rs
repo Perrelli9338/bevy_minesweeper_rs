de48adaf52212ea2d44633e3adbb76527c82e974
Perrelli9338 <prrmrc@gmail.com>
2024-09-29 00:59:28 +0200

    Insert camera dragging Changed grid limit to 200 x 200

---
+++ src/system/camera.rs
@@ -0,0 +1,53 @@
+use crate::AppState;
+use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
+
+pub struct CameraHandling;
+
+impl Plugin for CameraHandling {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            Update,
+            (handle_mouse, handle_touch).run_if(in_state(AppState::Playing)),
+        );
+    }
+}
+
+pub fn handle_mouse(
+    mut camera: Query<&mut Transform, With<Camera>>,
+    mouse_input: Res<ButtonInput<MouseButton>>,
+    window_primary_query: Query<&Window, With<PrimaryWindow>>,
+    mut cursor_moved_events: EventReader<CursorMoved>,
+) {
+    let Ok(window) = window_primary_query.get_single() else {
+        return;
+    };
+    if mouse_input.pressed(MouseButton::Middle) {
+        for event in cursor_moved_events.read() {
+            if let Some(last_position) = window.cursor_position() {
+                let delta = event.position - last_position;
+                for mut transform in camera.iter_mut() {
+                    transform.translation.x += delta.x;
+                    transform.translation.y -= delta.y;
+                }
+            }
+        }
+    }
+}
+
+pub fn handle_touch(mut camera: Query<&mut Transform, With<Camera>>, mut touches: Res<Touches>) {
+    if touches.iter().count() == 2 {
+        let mut fingers = touches
+            .iter()
+            .map(|touch| touch.position())
+            .collect::<Vec<_>>();
+        let delta = Vec2::new(
+            (fingers[0].x + fingers[1].x) / 2.0,
+            (fingers[0].y + fingers[1].y) / 2.0,
+        );
+        for mut transform in camera.iter_mut() {
+            transform.translation.x += delta.x;
+            transform.translation.y -= delta.y;
+        }
+    }
+}
