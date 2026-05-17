0c6b21fab6a718cb3017dacbadd24692bbbc3d24
Perrelli9338 <99440016+Perrelli9338@users.noreply.github.com>
2026-05-17 17:57:04 +0200

    feat: enhanced game timer

--- src/components/timer.rs
+++ src/components/timer.rs
@@ -1,4 +1,21 @@
 use bevy::prelude::*;
+use web_time::{Duration, Instant};
 
-#[derive(Resource, Deref, DerefMut)]
-pub(crate) struct GameTimer(pub Timer);
+#[derive(Resource)]
+pub(crate) struct GameTimer {
+    pub start_time: Instant,
+    pub duration: Duration,
+}
+
+impl GameTimer {
+    pub fn from_seconds(seconds: f32) -> Self {
+        Self {
+            start_time: Instant::now(),
+            duration: Duration::from_secs_f32(seconds),
+        }
+    }
+
+    pub fn finished(&self) -> bool {
+        Instant::now().duration_since(self.start_time) >= self.duration
+    }
+}
